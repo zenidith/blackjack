28459001c1fe8ac9abbf78e2552888c4b8a3001c
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-13 22:23:49 +1000

    added cardsEl and sumEl variables to display innertext of input

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 let firstCard = 10
-let secondCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// 2. Store the cards paragraph in a variable called cardsEl
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+    // 3. Render the cars on the page using this format -> "Cards: 10 4"
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
     if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
     }
-    console.log(message)
+    messageEl.textContent = message
 }
