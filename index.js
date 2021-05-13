5e0d35d980c80a1e0bbd5ddd37438a7a449e3c33
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-13 23:54:30 +1000

    added newCar()

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
-// 2. Store the cards paragraph in a variable called cardsEl
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    // 3. Render the cars on the page using this format -> "Cards: 10 4"
-    sumEl.textContent = "Sum: " + sum
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -23,4 +21,15 @@ function startGame() {
         isAlive = false
     }
     messageEl.textContent = message
+}
+
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 6
+    // 2. Add the new card to the sum variable
+    sum += card
+    // 3. Call startGame()
+    startGame()
 }
