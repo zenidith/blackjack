ccbdaca131f72163be86431199c14476c29868f9
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-16 16:53:43 +1000

    added logic for getRandomCard()

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ let cardsEl = document.getElementById("cards-el")
 
 
 function getRandomCard() {
-    return Math.floor ( Math.random()*13) + 1
-    console.log(getRandomCard)
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
 }
 
 function startGame() {
